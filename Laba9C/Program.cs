6ec48de0b34a0a2f581b24686c359d2eb2aad3bb
Sofiia <65614017+Wozniak456@users.noreply.github.com>
2020-12-29 12:57:37 +0200

    Add project files.

---
+++ Laba9C/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MyLib;
+
+namespace Laba9C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введiть кiлькiсть символiв, яку не має перевищувати слово: ");
+            int number_ = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine();
+            Console.Write("Введiть рядок: ");
+            string str_ = Console.ReadLine();
+            string n_str = FirstLab.Novi_str(number_, str_);
+            Console.WriteLine();
+            Console.WriteLine($"Рядок iз вилученням слiв, у яких символiв менше нiж {number_}: {n_str}");
+        }
+    }
+}
---
+++ MyLib/FirstLab.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyLib
+{
+    public static class FirstLab
+    {
+        public static string Novi_str(int number, string str)
+        {
+            string delimiterChars = "- ,.:!?;";
+            string ps = "", ns = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (delimiterChars.Contains(str[i]))
+                {
+                    if (ps.Length >= number)
+                    {
+                        ns += ps;
+                    }
+                    ns += str[i];
+                    ps = "";
+                }
+                else
+                {
+                    ps += str[i];
+                }
+            }
+            if (ps.Length >= number)
+            {
+                ns += ps;
+            }
+            return ns;
+        }
+    }
+}
